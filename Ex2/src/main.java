95f3379c7489b5e596da2f44d8e7686f0ed3a053
BarNahmias <92825016+BarNahmias@users.noreply.github.com>
2021-12-16 21:15:30 +0200

    Add files via upload

---
+++ Ex2/src/main.java
@@ -0,0 +1,19 @@
+import DG_GUI.GetGraph;
+import DG_GUI.Panel;
+import Ex2.My_Algo;
+
+
+
+public class main {
+    public static void main(String[] args) {
+        My_Algo a =new My_Algo();
+        Panel P=  new Panel(a);
+
+
+       if(args.length>0){
+            GetGraph g= new GetGraph(a,P);
+           String ans =args[0];
+             g.loadFromJsom(ans);
+    }
+    }
+}
